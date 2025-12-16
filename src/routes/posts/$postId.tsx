e89ddafd5097a152b48e6fb8548d02f183f0fd3c
Justin Fan <justin.fan@doordash.com>
2025-12-16 11:35:50 -0800

    formated

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,7 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/posts/$postId")({
-component: RouteComponent,
-});
+export const Route = createFileRoute('/posts/$postId')({
+  component: RouteComponent,
+})
 
-function RouteComponent() { return <div>Hello "/posts/$postId"!</div>; }
+function RouteComponent() {
+  return <div>Hello "/posts/$postId"!</div>
+}
