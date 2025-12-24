ab91e2a876e4df031fe7ae52169643d80a9566bd
Justin Fan <justin.fan@doordash.com>
2025-12-24 18:28:55 -0500

    got simple chat set up with a playground

---
+++ convex/agent.ts
@@ -0,0 +1,11 @@
+import { components } from "./_generated/api";
+import { Agent } from "@convex-dev/agent";
+import { openai } from "@ai-sdk/openai";
+
+export const agent = new Agent(components.agent, {
+  name: "My Agent",
+  languageModel: openai.chat("gpt-4o-mini"),
+  instructions: "You are a general chat ai assistant",
+  tools: {},
+  maxSteps: 3,
+});
